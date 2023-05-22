a876679a694d144f9cb6b0591ab59f3175d68a4e
parvtaneja <109197360+parvtaneja@users.noreply.github.com>
2023-05-22 14:14:42 +0530

    signup.js

--- signup.js
+++ signup.js
@@ -1,6 +1,6 @@
    document.getElementById("submit").onclick = function jsfunc(){
          Moengage.add_unique_user_id(document.getElementById("fname").value); 
-         Moengage.add_last_name(doument.getElementById("lname").value);
+         Moengage.add_last_name(document.getElementById("lname").value);
          Moengage.add_gender(document.getElementById("gender").value);
          Moengage.add_email(document.getElementById("email").value);
       
